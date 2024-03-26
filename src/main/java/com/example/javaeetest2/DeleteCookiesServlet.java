766c6ea1a20238932ebb88f302c70bc8f96d3aa5
Damir Ospanbek <ospanbekdamir1@gmail.com>
2024-03-26 18:08:07 +0600

    DeleteCookiesServlet.java class is for deleting cookies of user

--- src/main/java/com/example/javaeetest2/DeleteCookiesServlet.java
+++ src/main/java/com/example/javaeetest2/DeleteCookiesServlet.java
@@ -11,10 +11,12 @@ import java.io.IOException;
 public class DeleteCookiesServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        Cookie cookie = new Cookie("some_id","321");
-
+        Cookie cookie = new Cookie("some_id","");
+        Cookie cookie2 = new Cookie("some_name","Damirbek");
         cookie.setMaxAge(0);
+        cookie2.setMaxAge(0);
 
         resp.addCookie(cookie);
+        resp.addCookie(cookie2);
     }
 }
